ced5297d2a17016c6010e386963ab6da4cbd51f6
胡青杨 <i@do.codes>
2018-04-06 20:44:31 +0800

    Init

--- json_parse.js
+++ json_parse.js
@@ -1,208 +1,119 @@
-/*
-    json_parse.js
-    2016-05-02
 
-    Public Domain.
+let at;     // The index of the current character
+let ch;     // The current character
+let escapee = {
+    "\"": "\"",
+    "\\": "\\",
+    "/": "/",
+    b: "\b",
+    f: "\f",
+    n: "\n",
+    r: "\r",
+    t: "\t"
+};
+let text;
 
-    NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
-
-    This file creates a json_parse function.
-
-        json_parse(text, reviver)
-            This method parses a JSON text to produce an object or array.
-            It can throw a SyntaxError exception.
-
-            The optional reviver parameter is a function that can filter and
-            transform the results. It receives each of the keys and values,
-            and its return value is used instead of the original value.
-            If it returns what it received, then the structure is not modified.
-            If it returns undefined then the member is deleted.
-
-            Example:
-
-            // Parse the text. Values that look like ISO date strings will
-            // be converted to Date objects.
-
-            myData = json_parse(text, function (key, value) {
-                var a;
-                if (typeof value === "string") {
-                    a =
-/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
-                    if (a) {
-                        return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4],
-                            +a[5], +a[6]));
-                    }
-                }
-                return value;
-            });
-
-    This is a reference implementation. You are free to copy, modify, or
-    redistribute.
-
-    This code should be minified before deployment.
-    See http://javascript.crockford.com/jsmin.html
-
-    USE YOUR OWN COPY. IT IS EXTREMELY UNWISE TO LOAD CODE FROM SERVERS YOU DO
-    NOT CONTROL.
-*/
-
-/*jslint for */
-
-/*property
-    at, b, call, charAt, f, fromCharCode, hasOwnProperty, message, n, name,
-    prototype, push, r, t, text
-*/
-
-var json_parse = (function () {
-    "use strict";
-
-// This is a function that can parse a JSON text, producing a JavaScript
-// data structure. It is a simple, recursive descent parser. It does not use
-// eval or regular expressions, so it can be used as a model for implementing
-// a JSON parser in other languages.
-
-// We are defining the function inside of another function to avoid creating
-// global variables.
-
-    var at;     // The index of the current character
-    var ch;     // The current character
-    var escapee = {
-        "\"": "\"",
-        "\\": "\\",
-        "/": "/",
-        b: "\b",
-        f: "\f",
-        n: "\n",
-        r: "\r",
-        t: "\t"
+let error = function (m) {
+    throw {
+        name: "SyntaxError",
+        message: m,
+        at: at,
+        text: text
     };
-    var text;
+};
 
-    var error = function (m) {
+let next = function (c) {
+    if (c && c !== ch) {
+        error("Expected '" + c + "' instead of '" + ch + "'");
+    }
+    ch = text.charAt(at);
+    at += 1;
+    return ch;
+};
 
-// Call error when something is wrong.
+let number = function () {
+    let value;
+    let string = "";
 
-        throw {
-            name: "SyntaxError",
-            message: m,
-            at: at,
-            text: text
-        };
-    };
-
-    var next = function (c) {
-
-// If a c parameter is provided, verify that it matches the current character.
-
-        if (c && c !== ch) {
-            error("Expected '" + c + "' instead of '" + ch + "'");
+    if (ch === "-") {
+        string = "-";
+        next("-");
+    }
+    while (ch >= "0" && ch <= "9") {
+        string += ch;
+        next();
+    }
+    if (ch === ".") {
+        string += ".";
+        while (next() && ch >= "0" && ch <= "9") {
+            string += ch;
         }
-
-// Get the next character. When there are no more characters,
-// return the empty string.
-
-        ch = text.charAt(at);
-        at += 1;
-        return ch;
-    };
-
-    var number = function () {
-
-// Parse a number value.
-
-        var value;
-        var string = "";
-
-        if (ch === "-") {
-            string = "-";
-            next("-");
+    }
+    if (ch === "e" || ch === "E") {
+        string += ch;
+        next();
+        if (ch === "-" || ch === "+") {
+            string += ch;
+            next();
         }
         while (ch >= "0" && ch <= "9") {
             string += ch;
             next();
         }
-        if (ch === ".") {
-            string += ".";
-            while (next() && ch >= "0" && ch <= "9") {
-                string += ch;
-            }
-        }
-        if (ch === "e" || ch === "E") {
-            string += ch;
-            next();
-            if (ch === "-" || ch === "+") {
-                string += ch;
+    }
+    value = +string;
+    if (!isFinite(value)) {
+        error("Bad number");
+    } else {
+        return value;
+    }
+};
+
+let string = function () {
+    let hex;
+    let i;
+    let value = "";
+    let uffff;
+
+    if (ch === "\"") {
+        while (next()) {
+            if (ch === "\"") {
                 next();
+                return value;
             }
-            while (ch >= "0" && ch <= "9") {
-                string += ch;
+            if (ch === "\\") {
                 next();
-            }
-        }
-        value = +string;
-        if (!isFinite(value)) {
-            error("Bad number");
-        } else {
-            return value;
-        }
-    };
-
-    var string = function () {
-
-// Parse a string value.
-
-        var hex;
-        var i;
-        var value = "";
-        var uffff;
-
-// When parsing for string values, we must look for " and \ characters.
-
-        if (ch === "\"") {
-            while (next()) {
-                if (ch === "\"") {
-                    next();
-                    return value;
-                }
-                if (ch === "\\") {
-                    next();
-                    if (ch === "u") {
-                        uffff = 0;
-                        for (i = 0; i < 4; i += 1) {
-                            hex = parseInt(next(), 16);
-                            if (!isFinite(hex)) {
-                                break;
-                            }
-                            uffff = uffff * 16 + hex;
+                if (ch === "u") {
+                    uffff = 0;
+                    for (i = 0; i < 4; i += 1) {
+                        hex = parseInt(next(), 16);
+                        if (!isFinite(hex)) {
+                            break;
                         }
-                        value += String.fromCharCode(uffff);
-                    } else if (typeof escapee[ch] === "string") {
-                        value += escapee[ch];
-                    } else {
-                        break;
+                        uffff = uffff * 16 + hex;
                     }
+                    value += String.fromCharCode(uffff);
+                } else if (typeof escapee[ch] === "string") {
+                    value += escapee[ch];
                 } else {
-                    value += ch;
+                    break;
                 }
+            } else {
+                value += ch;
             }
         }
-        error("Bad string");
-    };
+    }
+    error("Bad string");
+};
 
-    var white = function () {
+let white = function () {
+    while (ch && ch <= " ") {
+        next();
+    }
+};
 
-// Skip whitespace.
-
-        while (ch && ch <= " ") {
-            next();
-        }
-    };
-
-    var word = function () {
-
-// true, false, or null.
-
-        switch (ch) {
+let word = function () {
+    switch (ch) {
         case "t":
             next("t");
             next("r");
@@ -222,80 +133,69 @@ var json_parse = (function () {
             next("l");
             next("l");
             return null;
+    }
+    error("Unexpected '" + ch + "'");
+};
+
+let value;
+
+let array = function () {
+    let arr = [];
+    if (ch === "[") {
+        next("[");
+        white();
+        if (ch === "]") {
+            next("]");
+            return arr;   // empty array
         }
-        error("Unexpected '" + ch + "'");
-    };
-
-    var value;  // Place holder for the value function.
-
-    var array = function () {
-
-// Parse an array value.
-
-        var arr = [];
-
-        if (ch === "[") {
-            next("[");
+        while (ch) {
+            arr.push(value());
             white();
             if (ch === "]") {
                 next("]");
-                return arr;   // empty array
+                return arr;
             }
-            while (ch) {
-                arr.push(value());
-                white();
-                if (ch === "]") {
-                    next("]");
-                    return arr;
-                }
-                next(",");
-                white();
-            }
+            next(",");
+            white();
         }
-        error("Bad array");
-    };
+    }
+    error("Bad array");
+};
 
-    var object = function () {
+let object = function () {
+    let key;
+    let obj = {};
 
-// Parse an object value.
-
-        var key;
-        var obj = {};
-
-        if (ch === "{") {
-            next("{");
+    if (ch === "{") {
+        next("{");
+        white();
+        if (ch === "}") {
+            next("}");
+            return obj;
+        }
+        while (ch) {
+            key = string();
+            white();
+            next(":");
+            if (Object.hasOwnProperty.call(obj, key)) {
+                error("Duplicate key '" + key + "'");
+            }
+            obj[key] = value();
             white();
             if (ch === "}") {
                 next("}");
-                return obj;   // empty object
+                return obj;
             }
-            while (ch) {
-                key = string();
-                white();
-                next(":");
-                if (Object.hasOwnProperty.call(obj, key)) {
-                    error("Duplicate key '" + key + "'");
-                }
-                obj[key] = value();
-                white();
-                if (ch === "}") {
-                    next("}");
-                    return obj;
-                }
-                next(",");
-                white();
-            }
+            next(",");
+            white();
         }
-        error("Bad object");
-    };
+    }
+    error("Bad object");
+};
 
-    value = function () {
-
-// Parse a JSON value. It could be an object, an array, a string, a number,
-// or a word.
-
-        white();
-        switch (ch) {
+value = function () {
+    white();
+    switch (ch) {
         case "{":
             return object();
         case "[":
@@ -308,49 +208,44 @@ var json_parse = (function () {
             return (ch >= "0" && ch <= "9")
                 ? number()
                 : word();
-        }
-    };
+    }
+};
 
-// Return the json_parse function. It will have access to all of the above
-// functions and variables.
 
-    return function (source, reviver) {
-        var result;
+function parse(source, reviver) {
+    let result;
 
-        text = source;
-        at = 0;
-        ch = " ";
-        result = value();
-        white();
-        if (ch) {
-            error("Syntax error");
-        }
+    text = source;
+    at = 0;
+    ch = " ";
+    result = value();
+    white();
+    if (ch) {
+        error("Syntax error");
+    }
 
-// If there is a reviver function, we recursively walk the new structure,
-// passing each name/value pair to the reviver function for possible
-// transformation, starting with a temporary root object that holds the result
-// in an empty key. If there is not a reviver function, we simply return the
-// result.
-
-        return (typeof reviver === "function")
-            ? (function walk(holder, key) {
-                var k;
-                var v;
-                var val = holder[key];
-                if (val && typeof val === "object") {
-                    for (k in val) {
-                        if (Object.prototype.hasOwnProperty.call(val, k)) {
-                            v = walk(val, k);
-                            if (v !== undefined) {
-                                val[k] = v;
-                            } else {
-                                delete val[k];
-                            }
+    return (typeof reviver === "function")
+        ? (function walk(holder, key) {
+            let k;
+            let v;
+            let val = holder[key];
+            if (val && typeof val === "object") {
+                for (k in val) {
+                    if (Object.prototype.hasOwnProperty.call(val, k)) {
+                        v = walk(val, k);
+                        if (v !== undefined) {
+                            val[k] = v;
+                        } else {
+                            delete val[k];
                         }
                     }
                 }
-                return reviver.call(holder, key, val);
-            }({"": result}, ""))
-            : result;
-    };
-}());
+            }
+            return reviver.call(holder, key, val);
+        }({"": result}, ""))
+        : result;
+};
+
+
+// export default parse;
+module.exports = parse;
