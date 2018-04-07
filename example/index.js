f8738460c327de5a19f11c639acc22b54dfc7aad
胡青杨 <i@do.codes>
2018-04-07 18:22:07 +0800

    Update example

---
+++ example/index.js
@@ -0,0 +1,9 @@
+
+import parse from '../dist';
+
+
+const source = `{"retCode":1441151997421.03334,"retData":{"respBodyMsg":{"users":[{"uid":2639271515,"class_id":0,"ts":1522659325},{"uid":144115199742103334,"class_id":0,"ts":1522728716,"uid_type":2}],"shards":1,"tid":2000006564,"cid":76629},"respHeader":{"time":324,"remote":"/10.60.38.128:35770","errmsg":"","errcode":0}},"retMsg":null}`;
+
+console.log(source);
+console.log(parse(source));
+console.log(JSON.parse(source));
---
+++ example/webpack.config.js
@@ -0,0 +1,37 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        example: path.join(__dirname, './index.js'),
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    // mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: false,
+        port: 2335,
+        open: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: './index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    target: 'web',
+};
